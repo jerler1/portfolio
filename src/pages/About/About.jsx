8297394e137f102e8e15951346eef1379fbcb706
Bunks <busk_1@yahoo.com>
2021-03-17 18:00:50 -0400

    added styling for the active class for links

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Container from "../../components/Container/Container";
 import Photo from "../../components/Photo/Photo";
+import CardHeader from "../../components/CardHeader/CardHeader";
 
 const About = () => {
   return (
     <>
+      <CardHeader>
 
+      </CardHeader>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
         velit libero unde. Nobis consequatur magni repellat quasi et impedit

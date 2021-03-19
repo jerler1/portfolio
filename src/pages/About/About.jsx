ae319b5734151ddda9b1f930bd7ee19d3956272f
Bunks <busk_1@yahoo.com>
2021-03-19 14:27:50 -0400

    styled the resume button

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -13,7 +13,9 @@ const About = () => {
       </CardHeader>
       <div className="columns p-5">
         <section className="column">
-          <h1 className="my-3 has-text-centered aboutHeaders is-size-5">About <span className="spanHeader">Me</span></h1>
+          <h1 className="my-3 has-text-centered aboutHeaders is-size-5">
+            About <span className="spanHeader">Me</span>
+          </h1>
           <p className="my-3">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Voluptatibus velit libero unde. Nobis consequatur magni repellat
@@ -28,15 +30,25 @@ const About = () => {
           </p>
         </section>
         <section className="column has-text-centered">
-          <h1 className="my-3 aboutHeaders is-size-5">My <span >Fundamentals</span></h1>
+          <h1 className="my-3 aboutHeaders is-size-5">
+            My <span className="spanHeader">Fundamentals</span>
+          </h1>
           <div className="skillIcons is-flex">
             {skills.map((icon) => {
-              return <div className="iconDiv"><figure>{icon.icon}</figure><p className="subtitle is-6 iconSubtitle">{icon.title}</p></div>;
+              return (
+                <div className="iconDiv">
+                  <figure>{icon.icon}</figure>
+                  <p className="subtitle is-6 iconSubtitle">{icon.title}</p>
+                </div>
+              );
             })}
           </div>
           <div>
-            <a href={resume} target="_blank" className="button">
-              Resume
+            <a href={resume} target="_blank" className="button is-info mt-3 resumeButton">
+              <span>Download Resume</span>
+              <span>
+              <i class="fas fa-file-export"></i>
+              </span>
             </a>
           </div>
         </section>

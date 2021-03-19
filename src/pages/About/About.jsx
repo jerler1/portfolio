b233ad475e3808ca0c89f1cf63497bb1329c30e9
Bunks <busk_1@yahoo.com>
2021-03-18 20:41:01 -0400

    refactored the icons and moved css for page into About.css

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Container from "../../components/Container/Container";
 import Photo from "../../components/Photo/Photo";
 import CardHeader from "../../components/CardHeader/CardHeader";
-import { headShot as Me, github, linkedIn } from "../../assets/";
+import skills from "./skills";
+import "./About.css";
 
 const About = () => {
   return (
@@ -11,8 +11,8 @@ const About = () => {
         <Photo />
       </CardHeader>
       <div className="columns p-5">
-        <div className="column">
-          <h1 className="my-3">About me</h1>
+        <section className="column">
+          <h1 className="my-3 has-text-centered">About me</h1>
           <p className="my-3">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Voluptatibus velit libero unde. Nobis consequatur magni repellat
@@ -25,24 +25,18 @@ const About = () => {
             hic ad, quas maiores vero nam deserunt quisquam tenetur animi, magni
             libero!
           </p>
-        </div>
-        <div className="column has-text-centered">
-          <div className="button">Resume</div>
-          <i className="devicon-mongodb-plain-wordmark devicon m-2"></i>
-          <i className="devicon-express-original devicon m-2"></i>
-          <i className="devicon-react-original-wordmark devicon m-2"></i>
-          <i className="devicon-nodejs-plain devicon m-2"></i>
-          <i className="devicon-javascript-plain devicon m-2"></i>
-          <i className="devicon-jquery-plain-wordmark devicon m-2"></i>
-          <i className="devicon-html5-plain-wordmark devicon m-2"></i>
-          <i className="devicon-mysql-plain-wordmark devicon m-2"></i>
-          <i className="devicon-handlebars-plain-wordmark devicon m-2"></i>
-          <i className="devicon-sequelize-plain-wordmark devicon m-2"></i>
-          <i className="devicon-bootstrap-plain-wordmark devicon m-2"></i>
-          <i className="devicon-css3-plain-wordmark devicon m-2"></i>
-          <i className="devicon-git-plain-wordmark devicon m-2"></i>
-          <i className="devicon-github-original-wordmark devicon m-2"></i>
-        </div>
+        </section>
+        <section className="column has-text-centered">
+          <h1 className="my-3">My Fundamentals</h1>
+          <div className="skillIcons is-flex">
+            {skills.map((icon) => {
+              return <figure>{icon.icon}</figure>;
+            })}
+          </div>
+          <div>
+            <div className="button">Resume</div>
+          </div>
+        </section>
       </div>
     </>
   );

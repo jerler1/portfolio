1b56121b07232d4909299094541881a75e19b02b
Bunks <busk_1@yahoo.com>
2021-03-25 00:48:20 -0400

    added contact css and migrated css over

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Contact.css";
 import emailjs, { init } from "emailjs-com";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";

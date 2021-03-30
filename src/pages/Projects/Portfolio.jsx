df33eafcf5e24b358beefb6e41aabc44d4ceca52
Bunks <busk_1@yahoo.com>
2021-03-30 01:52:59 -0400

    made a project component and transfered code into it

--- src/pages/Projects/Portfolio.jsx
+++ src/pages/Projects/Portfolio.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import works from "./ProjectInfo";
+import Project from "../../components/Project/Project";
+import Fade from "react-reveal/Fade";
 import "./Portfolio.css";
 import CardHeader from "../../components/CardHeader/CardHeader";
 
 const Portfolio = () => {
+  const [isShown, setIsShown] = useState(false);
+  const [projectList, setProjectList] = useState({});
+
+  useEffect(() => {
+    works.map((project) => {
+      setProjectList({ key: project.id, isShown: false });
+    });
+  }, []);
+
+  const handleReveal = (event) => {
+    console.log(projectList);
+    console.log("Reveal");
+  };
+  const handleHiding = (event) => {
+    console.log("leaving");
+  };
+
   return (
     <>
       <CardHeader headingName="Portfolio" />
@@ -18,27 +37,11 @@ const Portfolio = () => {
       <div className="columns is-flex is-multiline gridContainer m-0 has-text-center">
         {works.map((project) => {
           return (
-            <div
-              className="column is-full-mobile is-half-tablet is-one-third-desktop"
-              key={project.id}
-            >
-              <figure>
-                <img
-                  src={project.image}
-                  alt={project.alt}
-                  key={project.id}
-                  className="projectImage"
-                />
-                <figcaption className="transistion is-size-4 is-size-3-mobile is-size-3-tablet">
-                  {project.name}
-                </figcaption>
-                <button className="gitHub button transistion">GitHub</button>
-                <button className="liveSite button transistion">
-                  Live Site
-                </button>
-                <div className="overlay"></div>
-              </figure>
-            </div>
+            <Project
+              {...project}
+              handleHiding={handleHiding}
+              handleReveal={handleReveal}
+            />
           );
         })}
       </div>
